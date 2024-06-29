c12ec093186ec8d461b6d541eca000819052d87f
Amal <kvamal2255@gmail.com>
2024-06-29 23:03:25 +0530

    Initial Commit

---
+++ IosMovieAppDemo/IosMovieAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosMovieAppDemoApp.swift
+//  IosMovieAppDemo
+//
+//  Created by Amal k v on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IosMovieAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
